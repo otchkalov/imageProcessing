e85cc29b9be334721a48ec0c7ebd658ee851778a
oleksii.chkalov <tchkalov@gmail.com>
2024-01-11 13:47:04 +0200

    First commit

---
+++ Basics/imageRead.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Get image and display it
+img1 = plt.imread('Images/DSCN0453_e.JPG')
+plt.imshow(img1)
+plt.axis('off')
+plt.show()
+
+# Print ndarray properties
+print(type(img1))
+print(img1.shape)
+print(img1.ndim)
+print(img1.size)
+print(img1.dtype)
+print(img1.nbytes)
